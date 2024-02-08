419975cae874b7b5a7e9c3d9269b75a5887a6133
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2024-02-08 16:04:11 -0300

    debugged code

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -10,6 +10,8 @@ const Time = (props) => {
       <h3 style={cssCorPrimaria}>{props.nome}</h3>
       <div className='colaboradores'>
         {props.colaboradores.map(colaborador => <Colaborador
+          key={colaborador.nome}
+          corDeFundo={props.corPrimaria}
           nome={colaborador.nome}
           cargo={colaborador.cargo}
           imagem={colaborador.imagem}
